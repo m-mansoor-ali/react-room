fe2bf533845371334d9748b68adeaad688e4d1ca
M Mansoor Ali <muhammadmansoorali@outlook.com>
2019-01-26 12:21:07 +0500

    Updates

---
+++ precache-manifest.0e76f97dfc1061800072cc48cbc72709.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-room/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "69145f96cfdaef474ced",
+    "url": "/react-room/static/js/runtime~main.69145f96.js"
+  },
+  {
+    "revision": "a42b3e30933fdfc836ba",
+    "url": "/react-room/static/js/main.a42b3e30.chunk.js"
+  },
+  {
+    "revision": "f4e9df2526dd4be3e4be",
+    "url": "/react-room/static/js/1.f4e9df25.chunk.js"
+  },
+  {
+    "revision": "a42b3e30933fdfc836ba",
+    "url": "/react-room/static/css/main.bf8df283.chunk.css"
+  },
+  {
+    "revision": "a0c6bea2d759ad838c189e4879a00d43",
+    "url": "/react-room/index.html"
+  }
+];
